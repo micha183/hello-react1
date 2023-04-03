ad21870aac419e9dfb0be782b60ed2c8f6db839b
micha183 <micha183@msu.edu>
2023-04-03 18:06:02 -0400

    componenet lab push

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+const magazines = [
+  {id: 1, title: 'Architectural Digest', theme: 'architecture', isAvailable: true},
+  {id: 2, title: 'Dwell', theme: 'architecture', isAvailable: true},
+  {id: 3, title: 'Communication Arts', theme: 'design', isAvailable: false},
+]
+function ZineRack () {
+  const listZines = magazines.map(zine =>
+    <li
+      key={zine.id}
+      style={{
+        color: zine.isAvailable ? 'green' : 'red'
+      }}
+    >
+      {zine.title}
+    </li>
+  );
+  return (
+    <ul>{listZines}</ul>
+  )
+}
+
+const book = {
+  title: 'A Farewell to Arms',
+  author: 'Earnest Hemmingway',
+  published: '1929'
+};
+
+function DisplayBook () {
+  return (
+    <div>
+      <h3>{book.title} by {book.author} ({book.published})</h3>
+    </div>
+  )
+}
+
+function MagicButton () {
+  return (
+    <div>
+      <h3>This is a magic button</h3>
+      <button>MagicButton</button>
+    </div> 
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MagicButton />
+        <DisplayBook />
+        <ZineRack />
       </header>
     </div>
   );
